aff2663d93d27ce75abee8f87c24ffdd789011a0
At0mSsS-Cyber <akhiltom0274@gmail.com>
2024-07-10 20:09:58 +0530

    Changes added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
+import React from "react";
+import { render } from "react-snapshot";
+import { App } from "./app/App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+render(<App />, document.getElementById("root"));
